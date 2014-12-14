fd5d298ba442f59e97c716ee64d3c3e57169e3b9
{your user name} <{your email address}>
2014-12-13 23:06:36 -0800

    add build config

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,42 @@
 var gulp = require('gulp');
 
 require('require-dir')('./gulp');
+// Include plugins
+var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 
-gulp.task('default', ['clean'], function () {
+gulp.task('lint', function(){
+    return gulp.src('js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+gulp.task('sass', function(){
+    return gulp.src('scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('css'));
+});
+
+// Concatenate & Minify JS
+gulp.task('scripts', function(){
+    return gulp.src('js/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(rename('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
+
+// Watch Files for Changes
+gulp.task('watch', function(){
+    gulp.watch('js/*.js', ['lint', 'scripts']);
+    gulp.watch('scss/*.scss', ['sass']);
+});
+
+
+gulp.task('default', ['clean', 'lint', 'sass', 'scripts','watch'], function () {
     gulp.start('build');
 });
